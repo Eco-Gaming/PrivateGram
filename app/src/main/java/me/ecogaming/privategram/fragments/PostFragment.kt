88ee861ececfb8fa0ba084c4750d4c8b8a071bf5
Eco-Gaming <7ecogaming4@gmail.com>
2023-08-20 15:52:39 +0200

    add empty PostFragment

---
+++ app/src/main/java/me/ecogaming/privategram/fragments/PostFragment.kt
@@ -0,0 +1,36 @@
+package me.ecogaming.privategram.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import me.ecogaming.privategram.databinding.FragmentPostBinding
+
+class PostFragment : Fragment() {
+
+    private var _binding: FragmentPostBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentPostBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
