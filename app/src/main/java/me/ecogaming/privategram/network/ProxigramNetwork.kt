fa83baf5bb0349b5187dc1802cba0204c892ae49
Eco-Gaming <7ecogaming4@gmail.com>
2023-08-20 17:34:40 +0200

    change instance to locally hosted one

--- app/src/main/java/me/ecogaming/privategram/network/ProxigramNetwork.kt
+++ app/src/main/java/me/ecogaming/privategram/network/ProxigramNetwork.kt
@@ -7,7 +7,7 @@ object ProxigramNetwork {
 
     val retrofit: ProxigramApi by lazy {
         Retrofit.Builder()
-            .baseUrl("https://proxigram.privacyfrontends.repl.co")
+            .baseUrl("http://192.168.1.122:3000")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ProxigramApi::class.java)
